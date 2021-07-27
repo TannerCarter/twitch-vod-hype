ade1127f48e16d91b3deb6e49367fc3fecb464bb
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-26 20:01:34 -0700

    Configuration setup: Middleware, config.json, server.js, db, passport

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var path = require("path");
+var session = require("express-session");
+var passport = require("./config/passport");
+var compression = require("compression");
+
+// Express
+
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+// compression
+app.use(compression());
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+//public
+app.use(express.static("public"));
+
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+//Tima to add in routing once routes configured below this.
+
+//Routes Here
+
+// Syncing our sequelize models and then starting our Express app
+
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("Listening on port %s.", PORT, PORT);
+  });
+});
