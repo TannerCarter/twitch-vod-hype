b68d4d862a554a2feacc4b178dd7adb9fb84e1fb
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-26 20:28:03 -0700

    Added login model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+var bcrypt = require("bcryptjs");
+
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    preferences1: DataTypes.STRING,
+    preferences2: DataTypes.STRING,
+    preferences3: DataTypes.STRING,
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date(),
+    },
+  });
+
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+
+  return User;
+};
