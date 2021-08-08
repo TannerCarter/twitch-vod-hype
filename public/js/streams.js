645b65b2aa4a96e27c6ff47edac9167fdc034a31
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-08-08 06:55:27 -0700

    Added usernames from api to front end

--- public/js/streams.js
+++ public/js/streams.js
@@ -6,22 +6,45 @@ async function streams() {
   if (response.ok) {
     console.log("Tanner Is Goated!!!");
     console.log(response);
-    //Select both elements by ID - Streams and Clips
-    var streamName = document.querySelector("#streamName");
-    var streamClip = document.querySelector("#streamClip");
-    //Create elements for both id's selected - Streams and Clips
-    var nameP = document.createElement("p");
-    var clipP = document.createElement("p");
-    //Class names for P elements
-    nameP.className = "nameStyle";
-    clipP.className = "clipStyle";
-    //Append P Elements
-    streamName.appendChild(nameP);
-    streamClip.appendChild(clipP);
-    //Select Stream User from response
-    //Code Here
-    //Input selected data from response onto page in p element created
-    nameP.innerHTML = streamer;
+    response.json().then(function (data) {
+      console.log(data);
+      //Select elements by ID - Streams
+      var streamName1 = document.querySelector("#streamName1");
+      var streamName2 = document.querySelector("#streamName2");
+      var streamName3 = document.querySelector("#streamName3");
+      //Create elements for Streams
+      var nameP1 = document.createElement("p");
+      var nameP2 = document.createElement("p");
+      var nameP3 = document.createElement("p");
+      //Class names for stream elements
+      nameP1.className = "nameStyle1";
+      nameP2.className = "nameStyle2";
+      nameP3.className = "nameStyle3";
+      //Select Stream User from response
+      var streamer1 = data.data[0].user_name;
+      var streamer2 = data.data[1].user_name;
+      var streamer3 = data.data[2].user_name;
+
+      //Input selected data from response onto page in p element created
+      nameP1.innerHTML = streamer1;
+      nameP2.innerHTML = streamer2;
+      nameP3.innerHTML = streamer3;
+
+      //Append P Elements - Stream
+      streamName1.appendChild(nameP1);
+      streamName2.appendChild(nameP2);
+      streamName3.appendChild(nameP3);
+
+      //Select elements by ID - Clips
+      //var streamClip = document.querySelector("#streamClip");
+
+      //Create elements for Clips
+      //var clipP = document.createElement("p");
+
+      //clipP.className = "clipStyle";
+
+      //streamClip.appendChild(clipP);
+    });
   } else {
     alert(response.statusText);
   }
