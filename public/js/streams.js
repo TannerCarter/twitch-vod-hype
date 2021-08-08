1d944b6e1e77cefc9ec7c07daa341820b5b6c4ed
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-08-08 04:39:59 -0700

    Setting up stream data onto page

--- public/js/streams.js
+++ public/js/streams.js
@@ -5,6 +5,23 @@ async function streams() {
   });
   if (response.ok) {
     console.log("Tanner Is Goated!!!");
+    console.log(response);
+    //Select both elements by ID - Streams and Clips
+    var streamName = document.querySelector("#streamName");
+    var streamClip = document.querySelector("#streamClip");
+    //Create elements for both id's selected - Streams and Clips
+    var nameP = document.createElement("p");
+    var clipP = document.createElement("p");
+    //Class names for P elements
+    nameP.className = "nameStyle";
+    clipP.className = "clipStyle";
+    //Append P Elements
+    streamName.appendChild(nameP);
+    streamClip.appendChild(clipP);
+    //Select Stream User from response
+    //Code Here
+    //Input selected data from response onto page in p element created
+    nameP.innerHTML = streamer;
   } else {
     alert(response.statusText);
   }
